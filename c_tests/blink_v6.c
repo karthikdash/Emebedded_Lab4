0631b4a1bf3bdd10e14611a84d98731f8a72d40b
karthikdash <karthikeshwar.varma@gmail.com>
2018-10-29 12:09:18 -0400

    added changes:

--- c_tests/blink_v6.c
+++ c_tests/blink_v6.c
@@ -14,7 +14,7 @@ struct timespec t;
 
 int main (int argc, char** argv)
 {
-  int period = 500000;  // set initial period for delay in nsec
+  int period = 15000;  // set initial period for delay in nsec
   int PinValue = 0;  // hi/low indication of output Pin
   unsigned int current_sec, start_sec;
   float freq;
--- test_rt_skel_v1.c
+++ test_rt_skel_v1.c
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
 {
        struct timespec t;
        struct sched_param param;
-       int interval = 50000; /* 50us*/
+       int interval = 15000; /* 50us*/
        int PinValue = 0;  // used to toggle output pin
        float freq;
        int current_sec, start_sec;
@@ -45,7 +45,7 @@ int main(int argc, char* argv[])
        printf ( "   Frequency = %f\n", freq);
 
        wiringPiSetup();   // initialize WiringPi
-       pinMode (23, OUTPUT);  // Setup wPi pin 23 = GPIO 13 = OUTPUT
+       pinMode (25, OUTPUT);  // Setup wPi pin 23 = GPIO 13 = OUTPUT
 
        /* Set priority and process scheduler algorithm */
        param.sched_priority = MY_PRIORITY;
@@ -71,9 +71,17 @@ int main(int argc, char* argv[])
 
        while( current_sec < 100 ) {   // run the loop for 100 sec
                /* Use precision time to wait the specified amount of time` */
+
+	       digitalWrite(25, PinValue);
                clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
 
                // After sleep, do something!
+	       //digitalWrite(25, PinValue);
+
+     		PinValue = PinValue ^ 1;  // Flip the value of the output pin
+		//delay(0.01);
+
+	       //digitalWrite(25, 0);
 
                /* calculate next delay interval */
                t.tv_nsec += interval;
@@ -83,6 +91,8 @@ int main(int argc, char* argv[])
                    t.tv_sec++;
                    current_sec = t.tv_sec - start_sec;  // how many seconds since we started?
                }
+
+
         }
 }
 
